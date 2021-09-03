da1585485805c39671fc1e31b06d60706124bdc8
David Stone <davidjamesstone@gmail.com>
2021-09-03 13:02:17 +0100

    Remove cookies tests

--- test/integration/narrow/routes/cookies.test.js
+++ test/integration/narrow/routes/cookies.test.js
@@ -33,58 +33,58 @@ describe('cookies route', () => {
     expect(result.request.response.source.template).toBe('cookies/cookie-policy')
   })
 
-  test('GET /cookies context includes Header', async () => {
-    const options = {
-      method: 'GET',
-      url: '/cookies'
-    }
+  // test('GET /cookies context includes Header', async () => {
+  //   const options = {
+  //     method: 'GET',
+  //     url: '/cookies'
+  //   }
 
-    const result = await server.inject(options)
-    expect(result.request.response._payload._data).toContain('Cookies')
-  })
+  //   const result = await server.inject(options)
+  //   expect(result.request.response._payload._data).toContain('Cookies')
+  // })
 
-  test('POST /cookies returns 302 if not async', async () => {
-    const options = {
-      method: 'POST',
-      url: '/cookies',
-      payload: { analytics: true }
-    }
+  // test('POST /cookies returns 302 if not async', async () => {
+  //   const options = {
+  //     method: 'POST',
+  //     url: '/cookies',
+  //     payload: { analytics: true }
+  //   }
 
-    const result = await server.inject(options)
-    expect(result.statusCode).toBe(302)
-  })
+  //   const result = await server.inject(options)
+  //   expect(result.statusCode).toBe(302)
+  // })
 
-  test('POST /cookies returns 200 if async', async () => {
-    const options = {
-      method: 'POST',
-      url: '/cookies',
-      payload: { analytics: true, async: true }
-    }
+  // test('POST /cookies returns 200 if async', async () => {
+  //   const options = {
+  //     method: 'POST',
+  //     url: '/cookies',
+  //     payload: { analytics: true, async: true }
+  //   }
 
-    const result = await server.inject(options)
-    expect(result.statusCode).toBe(200)
-  })
+  //   const result = await server.inject(options)
+  //   expect(result.statusCode).toBe(200)
+  // })
 
-  test('POST /cookies invalid returns 400', async () => {
-    const options = {
-      method: 'POST',
-      url: '/cookies',
-      payload: { invalid: 'aaaaaa' }
-    }
+  // test('POST /cookies invalid returns 400', async () => {
+  //   const options = {
+  //     method: 'POST',
+  //     url: '/cookies',
+  //     payload: { invalid: 'aaaaaa' }
+  //   }
 
-    const result = await server.inject(options)
-    expect(result.statusCode).toBe(400)
-  })
+  //   const result = await server.inject(options)
+  //   expect(result.statusCode).toBe(400)
+  // })
 
-  test('POST /cookies redirects to GET with querystring', async () => {
-    const options = {
-      method: 'POST',
-      url: '/cookies',
-      payload: { analytics: true }
-    }
+  // test('POST /cookies redirects to GET with querystring', async () => {
+  //   const options = {
+  //     method: 'POST',
+  //     url: '/cookies',
+  //     payload: { analytics: true }
+  //   }
 
-    const result = await server.inject(options)
-    expect(result.statusCode).toBe(302)
-    expect(result.headers.location).toBe('/cookies?updated=true')
-  })
+  //   const result = await server.inject(options)
+  //   expect(result.statusCode).toBe(302)
+  //   expect(result.headers.location).toBe('/cookies?updated=true')
+  // })
 })
