49ebf8d1b91110519c96369aa189a28424872ddf
David Stone <davidjamesstone@gmail.com>
2021-09-27 16:02:44 +0100

    Update error messages

--- app/models/details-of-the-smell.js
+++ app/models/details-of-the-smell.js
@@ -1,12 +1,20 @@
 const joi = require('joi')
-const { BaseViewModel, schemaOptions } = require('./form')
+const { BaseViewModel, baseMessages } = require('./form')
 
 const DESCRIPTION_KEY = 'description'
 const DESCRIPTION_LABEL = 'How would you describe the smell?'
 const DESCRIPTION_LENGTH = 400
+const DESCRIPTION_MESSAGES = {
+  'string.max': `${DESCRIPTION_LABEL.slice(0, -1)} must be ${DESCRIPTION_LENGTH} characters or fewer`
+}
 
 const DATE_KEY = 'date'
 const DATE_LABEL = 'What date did you notice the smell?'
+const DATE_MESSAGES = {
+  'string.empty': 'Enter the date you noticed the smell',
+  'date.base': 'The date you noticed the smell must be a valid date',
+  'date.less': 'The date you noticed the smell must be today or in the past'
+}
 
 const TIME_KEY = 'time'
 const TIME_LABEL = 'What time of day did you notice the smell?'
@@ -17,12 +25,16 @@ const TIME_OPTIONS = {
     text: 'Use the 24 hour clock format for example 13:20'
   }
 }
+const TIME_MESSAGES = {
+  'string.empty': 'Enter the time of day you noticed the smell',
+  'string.pattern.base': 'The time of day you noticed the smell must be a valid time'
+}
 
 const schema = joi.object().keys({
-  [DESCRIPTION_KEY]: joi.string().max(DESCRIPTION_LENGTH).label(DESCRIPTION_LABEL).trim().required().allow(''),
-  [DATE_KEY]: joi.date().less('now').label(DATE_LABEL).required(),
-  [TIME_KEY]: joi.string().regex(TIME_PATTERN).label(TIME_LABEL).required()
-}).options(schemaOptions).required()
+  [DESCRIPTION_KEY]: joi.string().max(DESCRIPTION_LENGTH).label(DESCRIPTION_LABEL).trim().required().allow('').messages(DESCRIPTION_MESSAGES),
+  [DATE_KEY]: joi.date().less('now').label(DATE_LABEL).required().messages(DATE_MESSAGES),
+  [TIME_KEY]: joi.string().regex(TIME_PATTERN).label(TIME_LABEL).required().messages(TIME_MESSAGES)
+}).messages(baseMessages).required()
 
 class ViewModel extends BaseViewModel {
   constructor (data, err) {
