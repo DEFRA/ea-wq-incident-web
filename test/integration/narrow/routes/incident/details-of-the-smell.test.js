4243cdc9dea4b7e9f3f3ee6ba3df1a85b0b67154
David Stone <davidjamesstone@gmail.com>
2021-09-28 12:23:31 +0100

    Fix tests

--- test/integration/narrow/routes/incident/details-of-the-smell.test.js
+++ test/integration/narrow/routes/incident/details-of-the-smell.test.js
@@ -3,11 +3,15 @@ describe('Experience test', () => {
   let server
   let sessionHandler
   let mockSession = {}
+  let notify
 
   function createMocks () {
     jest.mock('../../../../../app/services/session-handler')
     sessionHandler = require('../../../../../app/services/session-handler')
     sessionHandler.get.mockImplementation(() => mockSession)
+    jest.mock('../../../../../app/services/notify')
+    notify = require('../../../../../app/services/notify')
+    notify.sendEmail.mockImplementation(jest.fn().mockResolvedValue({ result: true }))
   }
 
   beforeAll(async () => {
@@ -35,8 +39,51 @@ describe('Experience test', () => {
     expect(response.statusCode).toBe(200)
   })
 
+  test('POST /details-of-the-smell route returns 302', async () => {
+    mockSession = {
+      firstName: 'Mr',
+      lastName: 'Test',
+      addressLine1: 'Institute of test',
+      addressLine2: '',
+      townOrCity: 'Teston',
+      county: 'Testshire',
+      postcode: 'T3 5TR',
+      email: 'tester@test.com',
+      phonenumber: '1471',
+      strength: 'strong smell that may make your hair or clothes smell',
+      atHome: 'No',
+      location: 'elsewhere',
+      description: 'bad',
+      date: '2021-08-25T00:00:00.000Z',
+      time: '14:35'
+    }
+
+    const options = {
+      method: 'POST',
+      url: '/details-of-the-smell',
+      payload: {
+        description: 'test description',
+        date: '2017-08-25',
+        time: '14:35'
+      }
+    }
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(302)
+  })
+
   afterEach(async () => {
     jest.clearAllMocks()
     await server.stop()
   })
 })
+
+// test('GET /confirmation with valid session data return 200', async () => {
+//   const options = {
+//     method: 'GET',
+//     url: '/confirmation'
+//   }
+
+//   const response = await server.inject(options)
+//   expect(response.statusCode).toBe(200)
+// })
