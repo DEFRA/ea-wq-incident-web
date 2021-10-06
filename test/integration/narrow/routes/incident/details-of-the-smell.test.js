e8c2266913c3096c186b3f384870f9c53ccb5b3d
David Stone <davidjamesstone@gmail.com>
2021-10-06 15:49:52 +0100

    Fix failing test

--- test/integration/narrow/routes/incident/details-of-the-smell.test.js
+++ test/integration/narrow/routes/incident/details-of-the-smell.test.js
@@ -58,7 +58,7 @@ describe('Experience test', () => {
       method: 'POST',
       url: '/details-of-the-smell',
       payload: {
-        date: new Date().toISOString(),
+        date: '2021-08-25',
         hour: 14,
         minute: 35
       }
